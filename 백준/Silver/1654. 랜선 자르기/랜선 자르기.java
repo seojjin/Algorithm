b57d32e207dcf653b9b5b1a3628b64792ee38551
Park Seojin <002seojin@gmail.com>
2025-12-06 11:15:22 +0900

    [Silver II] Title: 랜선 자르기, Time: 160 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int k, n;
+	static int[] lines;
+	static long ans, maxLen;
+	
+
+	public static void main(String[] args) throws Exception {
+		input();
+		paramatricSearch();
+		System.out.println(ans);
+	}
+
+
+	private static void paramatricSearch() {
+		long left = 1;
+		long right = maxLen; // 가장 작은 랜선을 오른쪽 기준으로 삼음 
+		while(left<=right) {
+			long mid = (left + right)/2;
+			long cnt=0;
+			// 랜선 자르기 
+			for(int i=0;i<k;i++) {
+				cnt+=lines[i]/mid;
+			}
+			if(cnt >= n) {
+				left = mid+1;
+				ans = mid;
+			}else {
+				right = mid-1;
+			}
+		}
+		
+	}
+
+
+	private static void input() throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		k = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
+		lines = new int[k];
+		maxLen = -1;
+		for(int i=0;i<k;i++) {
+			lines[i] = Integer.parseInt(br.readLine());
+			maxLen = Math.max(maxLen, lines[i]);
+		}
+	}
+
+}
