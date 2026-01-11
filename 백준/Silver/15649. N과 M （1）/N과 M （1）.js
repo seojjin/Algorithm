24d9d8e02d8fc0426ac40823e6633f85aaa7f1fc
Park Seojin <002seojin@gmail.com>
2026-01-11 19:19:14 +0900

    [Silver III] Title: N과 M (1), Time: 180 ms, Memory: 17988 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -0,0 +1,58 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = __importStar(require("fs"));
+const input = fs.readFileSync(0, "utf8").trim().split(/\s+/);
+const n = Number(input[0]);
+const m = Number(input[1]);
+const visit = Array(n + 1).fill(false);
+const res = Array(m).fill(0);
+let out = [];
+const perm = function (depth) {
+    if (depth >= m) {
+        out.push(res.join(" "));
+        return;
+    }
+    for (let i = 1; i <= n; i++) {
+        if (visit[i])
+            continue;
+        res[depth] = i;
+        visit[i] = true;
+        perm(depth + 1);
+        visit[i] = false;
+    }
+};
+perm(0);
+console.log(out.join("\n"));
